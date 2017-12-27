7c3f8118ce0fbe3c3adfd1bcb8a57fac9b759e75
Yehia Saleh <yehia.saleh@yale.edu>
2017-12-26 23:17:14 -0500

    Callbacks

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+	.options({
+		a: {
+			describe: 'Address to fetch weather',
+			demand: true,
+			alias: 'address',
+			string: true
+		}
+	})
+	.help()
+	.alias('help', 'h')
+	.argv;
+
+const address = argv.address;
+const encodedAddress = encodeURIComponent(address);
 
 request({
-	url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+	url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
 	json: true
 }, (error, response, body) => {
-	console.log(body);
+	if (error) {
+		console.log('Unable to connect to Google servers.');
+	} else if (body.status === 'ZERO_RESULTS') {
+		console.log('Invalid address');
+	} else if (body.status === 'OK') {
+		console.log(`Address: ${body.results[0].formatted_address}`);
+		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+		console.log(`Longitude: ${body.results[0].geometry.location.lng}`);	
+	}
 });
